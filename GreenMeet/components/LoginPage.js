2e3c887a8362fe5bf4621936064c0a5be10c84de
antonfoch <anton.foch@epitech.digital>
2023-09-26 11:17:04 +0200

    FIX: Main router

--- GreenMeet/components/LoginPage.js
+++ GreenMeet/components/LoginPage.js
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { View, ImageBackground, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import * as Font from 'expo-font';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Accueil() {
   const [identifiant, setIdentifiant] = useState('');
   const [motDePasse, setMotDePasse] = useState('');
+  const navigation = useNavigation();
 
   const handleConnexion = () => {
     // Gérer l'action lors de l'appui sur le bouton Connexion
     console.log('Identifiant:', identifiant);
     console.log('Mot de passe:', motDePasse);
-};
+  };
 
-    return (
+  return (
     <View style={styles.accueil}>
       <ImageBackground
         style={styles.back_acceuil}
-        source={require('./assets/components/background.png')}
+        source={require('../assets/background.png')}
       />
       <ImageBackground
         style={styles.logo}
-        source={require('./assets/components/image34.png')}
+        source={require('../assets/image34.png')}
       />
       <View style={styles.container}>
         <Text style={[styles.customFontText, { fontFamily: 'Mulish-Bold' }]}>
@@ -60,7 +62,7 @@ export default function Accueil() {
       <TouchableOpacity
         style={styles.button2}
         onPress={() => {
-          // Gérer l'action lors de l'appui sur le bouton Créer un compte
+          navigation.navigate('Signup');
         }}
       >
         <View style={styles.border2} />
@@ -80,20 +82,20 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(198, 195, 195, 1)",
     alignItems: "flex-start",
     rowGap: 0
-},
+  },
   customFontText: {
     fontFamily: 'Mulish', // Utilisez le nom que vous avez spécifié lors du chargement de la police
     fontSize: 25, // Ajoutez d'autres styles selon vos besoins
     color: 'white',
-},
+  },
   container: {
     position: "absolute",
     flexShrink: 0,
     top: 100,
     right: 70,
     fontSize: 600,
-},
-    back_acceuil: {
+  },
+  back_acceuil: {
     position: "absolute",
     flexShrink: 0,
     top: 0,
@@ -101,7 +103,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     height: 850,
-},
+  },
   logo: {
     position: "absolute",
     flexShrink: 0,
@@ -109,8 +111,8 @@ const styles = StyleSheet.create({
     right: 129,
     bottom: 12,
     left: 138
-},
-    title: {
+  },
+  title: {
     position: "absolute",
     flexShrink: 0,
     top: 82,
@@ -123,24 +125,24 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: "700",
     letterSpacing: 0.10867942869663239
-},
-    group5: {
+  },
+  group5: {
     position: "absolute",
     flexShrink: 0,
     top: 280,
     height: 44,
     left: 33,
     width: 332,
-},
-    group6: {
+  },
+  group6: {
     position: "absolute",
     flexShrink: 0,
     top: 387,
     height: 44,
     left: 32,
     width: 334
-},
-    subtitle: {
+  },
+  subtitle: {
     position: "absolute",
     flexShrink: 0,
     top: 14,
@@ -153,22 +155,22 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "400",
     letterSpacing: 0.10867942869663239
-},
-    textInput: {
-      flexShrink: 0,
-      width: 330,
-      height: 50,
-      marginVertical: -10,
-      backgroundColor: "white",
-      borderWidth: 2,
-      borderColor: "balck",
-      borderRadius: 20,
-},
-    identifiantLabel: {
-        // Styles du label Identifiant
-},
-// Styles pour le bouton Connexion
-    button: {
+  },
+  textInput: {
+    flexShrink: 0,
+    width: 330,
+    height: 50,
+    marginVertical: -10,
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "balck",
+    borderRadius: 20,
+  },
+  identifiantLabel: {
+    // Styles du label Identifiant
+  },
+  // Styles pour le bouton Connexion
+  button: {
     height: 50,
     marginVertical: 10,
     alignItems: "center",
@@ -179,17 +181,17 @@ const styles = StyleSheet.create({
     left: 135,
     width: 100,
     height: 17,
-},
-    buttonText: {
+  },
+  buttonText: {
     textAlign: "center",
     color: "white",
     fontFamily: "Mulish",
     fontSize: 14,
     fontWeight: "700",
     letterSpacing: 0.10867942869663239,
-},
-// Styles pour le bouton Créer un compte
-    buttonText2: {
+  },
+  // Styles pour le bouton Créer un compte
+  buttonText2: {
     textAlign: "center",
     color: "white",
     fontFamily: "Mulish",
@@ -200,8 +202,8 @@ const styles = StyleSheet.create({
     left: 85,
     width: 200,
     height: 17,
-},
-    border: {
+  },
+  border: {
     position: "absolute",
     flexShrink: 0,
     right: 0,
@@ -209,8 +211,8 @@ const styles = StyleSheet.create({
     height: 50,
     backgroundColor: "rgba(87, 128, 250, 1)",
     borderRadius: 10
-},
-    _title: {
+  },
+  _title: {
     position: "absolute",
     flexShrink: 0,
     top: 17,
@@ -223,8 +225,8 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "700",
     letterSpacing: 0.10867942869663239
-},
-    __title: {
+  },
+  __title: {
     position: "absolute",
     flexShrink: 0,
     top: 600,
@@ -237,5 +239,5 @@ const styles = StyleSheet.create({
     fontSize: 13,
     fontWeight: "800",
     letterSpacing: 0.10867942869663239
-}
+  }
 })
