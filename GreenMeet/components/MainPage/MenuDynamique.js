b2f6e01e5a453f9c480d7bdd0588432699576e7d
nuts <esteban.baigts@epitech.eu>
2023-09-26 19:23:28 +0200

    {add} menu dynamique

---
+++ GreenMeet/components/MainPage/MenuDynamique.js
@@ -0,0 +1,8 @@
+import MainPage from './MainPage';
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, TouchableOpacity, TextInput, Text } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
+export function Menu() {
+    
+}
