d973f1aa7aae021318837c0533147d6fbfa71a50
nuts <esteban.baigts@epitech.eu>
2023-09-26 10:32:47 +0200

    {ADD} page dacceuil finit, on commence le home avec map etc

---
+++ GreenMeet/Acceuil.js
@@ -0,0 +1,241 @@
+import React, { useState, useEffect } from 'react';
+import { View, ImageBackground, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import * as Font from 'expo-font';
+
+export default function Accueil() {
+  const [identifiant, setIdentifiant] = useState('');
+  const [motDePasse, setMotDePasse] = useState('');
+
+  const handleConnexion = () => {
+    // Gérer l'action lors de l'appui sur le bouton Connexion
+    console.log('Identifiant:', identifiant);
+    console.log('Mot de passe:', motDePasse);
+};
+
+    return (
+    <View style={styles.accueil}>
+      <ImageBackground
+        style={styles.back_acceuil}
+        source={require('./assets/components/background.png')}
+      />
+      <ImageBackground
+        style={styles.logo}
+        source={require('./assets/components/image34.png')}
+      />
+      <View style={styles.container}>
+        <Text style={[styles.customFontText, { fontFamily: 'Mulish-Bold' }]}>
+          {`Bienvenue sur GreenMeet`}
+        </Text>
+      </View>
+      <View style={styles.group5}>
+        <View style={styles.rectangle68} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Saisissez votre identifiant"
+          onChangeText={text => setIdentifiant(text)}
+          value={identifiant}
+        />
+      </View>
+      <View style={styles.group6}>
+        <View style={styles.rectangle67} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Saisissez votre mot de passe"
+          secureTextEntry={true} // Pour masquer le texte du mot de passe
+          onChangeText={text => setMotDePasse(text)}
+          value={motDePasse}
+        />
+      </View>
+      {/* Bouton Connexion */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleConnexion}
+      >
+        <View style={styles.border} />
+        <Text style={styles.buttonText}>
+          {`Connexion`}
+        </Text>
+      </TouchableOpacity>
+      {/* Bouton Créer un compte */}
+      <TouchableOpacity
+        style={styles.button2}
+        onPress={() => {
+          // Gérer l'action lors de l'appui sur le bouton Créer un compte
+        }}
+      >
+        <View style={styles.border2} />
+        <Text style={styles.buttonText2}>
+          {`Me créer un compte`}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  accueil: {
+    flexShrink: 0,
+    height: 844,
+    width: 390,
+    backgroundColor: "rgba(198, 195, 195, 1)",
+    alignItems: "flex-start",
+    rowGap: 0
+},
+  customFontText: {
+    fontFamily: 'Mulish', // Utilisez le nom que vous avez spécifié lors du chargement de la police
+    fontSize: 25, // Ajoutez d'autres styles selon vos besoins
+    color: 'white',
+},
+  container: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 100,
+    right: 70,
+    fontSize: 600,
+},
+    back_acceuil: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    height: 850,
+},
+  logo: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 722,
+    right: 129,
+    bottom: 12,
+    left: 138
+},
+    title: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 82,
+    right: 0,
+    left: 0,
+    height: 40,
+    textAlign: "center",
+    color: "white",
+    fontFamily: "Mulish",
+    fontSize: 32,
+    fontWeight: "700",
+    letterSpacing: 0.10867942869663239
+},
+    group5: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 280,
+    height: 44,
+    left: 33,
+    width: 332,
+},
+    group6: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 387,
+    height: 44,
+    left: 32,
+    width: 334
+},
+    subtitle: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 14,
+    left: 120,
+    width: 87,
+    height: 17,
+    textAlign: "center",
+    color: "rgba(123, 123, 123, 1)",
+    fontFamily: "Mulish",
+    fontSize: 14,
+    fontWeight: "400",
+    letterSpacing: 0.10867942869663239
+},
+    textInput: {
+      flexShrink: 0,
+      width: 330,
+      height: 50,
+      marginVertical: -10,
+      backgroundColor: "white",
+      borderWidth: 2,
+      borderColor: "balck",
+      borderRadius: 20,
+},
+    identifiantLabel: {
+        // Styles du label Identifiant
+},
+// Styles pour le bouton Connexion
+    button: {
+    height: 50,
+    marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(87, 128, 250, 1)",
+    borderRadius: 10,
+    top: 500,
+    left: 135,
+    width: 100,
+    height: 17,
+},
+    buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontFamily: "Mulish",
+    fontSize: 14,
+    fontWeight: "700",
+    letterSpacing: 0.10867942869663239,
+},
+// Styles pour le bouton Créer un compte
+    buttonText2: {
+    textAlign: "center",
+    color: "white",
+    fontFamily: "Mulish",
+    fontSize: 14,
+    fontWeight: "700",
+    letterSpacing: 0.10867942869663239,
+    top: 530,
+    left: 85,
+    width: 200,
+    height: 17,
+},
+    border: {
+    position: "absolute",
+    flexShrink: 0,
+    right: 0,
+    left: 0,
+    height: 50,
+    backgroundColor: "rgba(87, 128, 250, 1)",
+    borderRadius: 10
+},
+    _title: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 17,
+    right: 16,
+    left: 15,
+    height: 16,
+    textAlign: "center",
+    color: "rgba(255, 255, 255, 1)",
+    fontFamily: "Mulish",
+    fontSize: 14,
+    fontWeight: "700",
+    letterSpacing: 0.10867942869663239
+},
+    __title: {
+    position: "absolute",
+    flexShrink: 0,
+    top: 600,
+    right: 115,
+    left: 115,
+    height: 16,
+    textAlign: "center",
+    color: "rgba(255, 255, 255, 1)",
+    fontFamily: "Mulish",
+    fontSize: 13,
+    fontWeight: "800",
+    letterSpacing: 0.10867942869663239
+}
+})
--- GreenMeet/App.js
+++ GreenMeet/App.js
@@ -1,241 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import { View, ImageBackground, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-import * as Font from 'expo-font';
+import React from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
-export default function Accueil() {
-  const [identifiant, setIdentifiant] = useState('');
-  const [motDePasse, setMotDePasse] = useState('');
+import Btn_loc from './assets/components/ic_loc.png'; // Importez Ic_loc depuis le fichier approprié
+import Ic_gesture from './assets/components/ic_gesture.png'; // Importez Ic_gesture depuis le fichier approprié
+import Ic_menu from './assets/components/ic_menu.png'; // Importez Ic_menu depuis le fichier approprié
 
-  const handleConnexion = () => {
-    // Gérer l'action lors de l'appui sur le bouton Connexion
-    console.log('Identifiant:', identifiant);
-    console.log('Mot de passe:', motDePasse);
-};
-
+export default function Home() {
     return (
-    <View style={styles.accueil}>
-      <ImageBackground
-        style={styles.back_acceuil}
-        source={require('./assets/background.png')}
-      />
-      <ImageBackground
-        style={styles.logo}
-        source={require('./assets/image34.png')}
-      />
-      <View style={styles.container}>
-        <Text style={[styles.customFontText, { fontFamily: 'Mulish-Bold' }]}>
-          {`Bienvenue sur GreenMeet`}
-        </Text>
-      </View>
-      <View style={styles.group5}>
-        <View style={styles.rectangle68} />
-        <TextInput
-          style={styles.textInput}
-          placeholder="Saisissez votre identifiant"
-          onChangeText={text => setIdentifiant(text)}
-          value={identifiant}
-        />
-      </View>
-      <View style={styles.group6}>
-        <View style={styles.rectangle67} />
-        <TextInput
-          style={styles.textInput}
-          placeholder="Saisissez votre mot de passe"
-          secureTextEntry={true} // Pour masquer le texte du mot de passe
-          onChangeText={text => setMotDePasse(text)}
-          value={motDePasse}
-        />
-      </View>
-      {/* Bouton Connexion */}
-      <TouchableOpacity
-        style={styles.button}
-        onPress={handleConnexion}
-      >
-        <View style={styles.border} />
-        <Text style={styles.buttonText}>
-          {`Connexion`}
-        </Text>
-      </TouchableOpacity>
-      {/* Bouton Créer un compte */}
-      <TouchableOpacity
-        style={styles.button2}
-        onPress={() => {
-          // Gérer l'action lors de l'appui sur le bouton Créer un compte
-        }}
-      >
-        <View style={styles.border2} />
-        <Text style={styles.buttonText2}>
-          {`Me créer un compte`}
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
+        <View style={styles.container}>
+            <Image source={Btn_loc} style={styles.btnLocImage} /> {/* Utilisez l'image Ic_loc */}
+            <View style={styles.btn_loc}>
+                {/* ... Autres éléments ... */}
+            </View>
+            {/* ... Autres éléments ... */}
+            
+            <Image source={Ic_gesture} style={styles.icGestureImage} /> {/* Utilisez l'image Ic_gesture */}
+            
+            <View style={styles.navbar_x}>
+                <View style={styles.navbtn}>
+                    <Image source={Ic_menu} style={styles.icMenuImage} /> {/* Utilisez l'image Ic_menu */}
+                </View>
+            </View>
+            
+            {/* Rectangle 1223 */}
+            <View style={styles.rectangle1223}>
+                <Text style={styles.rechercherdansGreenMeet}>
+                    {`Rechercher dans GreenMeet`}
+                </Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  accueil: {
-    flexShrink: 0,
-    height: 844,
-    width: 390,
-    backgroundColor: "rgba(198, 195, 195, 1)",
-    alignItems: "flex-start",
-    rowGap: 0
-},
-  customFontText: {
-    fontFamily: 'Mulish', // Utilisez le nom que vous avez spécifié lors du chargement de la police
-    fontSize: 25, // Ajoutez d'autres styles selon vos besoins
-    color: 'white',
-},
-  container: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 100,
-    right: 70,
-    fontSize: 600,
-},
-    back_acceuil: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    height: 850,
-},
-  logo: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 722,
-    right: 129,
-    bottom: 12,
-    left: 138
-},
-    title: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 82,
-    right: 0,
-    left: 0,
-    height: 40,
-    textAlign: "center",
-    color: "white",
-    fontFamily: "Mulish",
-    fontSize: 32,
-    fontWeight: "700",
-    letterSpacing: 0.10867942869663239
-},
-    group5: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 280,
-    height: 44,
-    left: 33,
-    width: 332,
-},
-    group6: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 387,
-    height: 44,
-    left: 32,
-    width: 334
-},
-    subtitle: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 14,
-    left: 120,
-    width: 87,
-    height: 17,
-    textAlign: "center",
-    color: "rgba(123, 123, 123, 1)",
-    fontFamily: "Mulish",
-    fontSize: 14,
-    fontWeight: "400",
-    letterSpacing: 0.10867942869663239
-},
-    textInput: {
-      flexShrink: 0,
-      width: 330,
-      height: 50,
-      marginVertical: -10,
-      backgroundColor: "white",
-      borderWidth: 2,
-      borderColor: "balck",
-      borderRadius: 20,
-},
-    identifiantLabel: {
-        // Styles du label Identifiant
-},
-// Styles pour le bouton Connexion
-    button: {
-    height: 50,
-    marginVertical: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "rgba(87, 128, 250, 1)",
-    borderRadius: 10,
-    top: 500,
-    left: 135,
-    width: 100,
-    height: 17,
-},
-    buttonText: {
-    textAlign: "center",
-    color: "white",
-    fontFamily: "Mulish",
-    fontSize: 14,
-    fontWeight: "700",
-    letterSpacing: 0.10867942869663239,
-},
-// Styles pour le bouton Créer un compte
-    buttonText2: {
-    textAlign: "center",
-    color: "white",
-    fontFamily: "Mulish",
-    fontSize: 14,
-    fontWeight: "700",
-    letterSpacing: 0.10867942869663239,
-    top: 530,
-    left: 85,
-    width: 200,
-    height: 17,
-},
-    border: {
-    position: "absolute",
-    flexShrink: 0,
-    right: 0,
-    left: 0,
-    height: 50,
-    backgroundColor: "rgba(87, 128, 250, 1)",
-    borderRadius: 10
-},
-    _title: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 17,
-    right: 16,
-    left: 15,
-    height: 16,
-    textAlign: "center",
-    color: "rgba(255, 255, 255, 1)",
-    fontFamily: "Mulish",
-    fontSize: 14,
-    fontWeight: "700",
-    letterSpacing: 0.10867942869663239
-},
-    __title: {
-    position: "absolute",
-    flexShrink: 0,
-    top: 600,
-    right: 115,
-    left: 115,
-    height: 16,
-    textAlign: "center",
-    color: "rgba(255, 255, 255, 1)",
-    fontFamily: "Mulish",
-    fontSize: 13,
-    fontWeight: "800",
-    letterSpacing: 0.10867942869663239
-}
-})
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    btnLocImage: {
+        width: 100,
+        height: 100,
+        marginBottom: 10,
+    },
+    icGestureImage: {
+        width: 50,
+        height: 50,
+        marginBottom: 10,
+    },
+    icMenuImage: {
+        width: 50,
+        height: 50,
+        marginBottom: 10,
+    },
+    rectangle1223: {
+        width: 303,
+        height: 37,
+        backgroundColor: "rgba(217, 217, 217, 1)",
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative', // Ajoutez cette ligne pour permettre le positionnement absolu
+    },
+    rechercherdansGreenMeet: {
+        fontSize: 15,
+        fontFamily: 'Mulish',
+        textAlign: "center",
+    },
+    // Ajoutez d'autres styles selon vos besoins
+});
