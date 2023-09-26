f29a189835a19865fcf97f96447255275adf014c
antonfoch <anton.foch@epitech.digital>
2023-09-26 16:20:57 +0200

    FIX: appjs

--- GreenMeet/components/AppNavigator.js
+++ GreenMeet/components/AppNavigator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import LoginPage from './LoginPage';
 import SignupPage from './SignupPage';
